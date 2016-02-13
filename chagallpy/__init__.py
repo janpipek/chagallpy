f2e25a20a6c2e6be8bfb08459eb2c068597c8ee9
Jan Pipek <jan.pipek@gmail.com>
2016-02-13 13:24:40 +0100

    ImageInfo

--- chagallpy/__init__.py
+++ chagallpy/__init__.py
@@ -1,1 +1,13 @@
-__version__ = str('0.2.9')
+__version__ = str('0.1.0')
+
+from . import image_info
+
+def generate():
+    # 1. Get list of pictures
+    # 2. Read all possible information about them
+    # 3. Sort them
+    # 4. Create
+    #    a) thumbnails
+    #    b) pages
+    #    c) album page
+    pass
---
+++ chagallpy/image_info.py
@@ -0,0 +1,41 @@
+import os
+import datetime
+
+
+class ImageInfo(object):
+    """Container for image information.
+
+    Objects of this class are passed in the workflow.
+    """
+    def __init__(self, path):
+        self.path = path
+        self._exif_data = {}
+        self._meta_data = {}
+
+    @property
+    def exif_data(self):
+        return self._exif_data
+
+    @exif_data.setter
+    def exif_data(self, value):
+        self._exif_data = value
+
+    @property
+    def meta_data(self):
+        return self._meta_data
+
+    @meta_data.setter
+    def meta_data(self, value):
+        self._meta_data = value
+
+    @property
+    def date_time(self):
+        """Best available data time info.
+
+        :rtype datetime.datetime
+        """
+        if "DateTime" in self._exif_data:
+            exiftime = self._exif_data["DateTime"]
+            return datetime.datetime.strptime(exiftime, "%Y:%m:%d %H:%M:%S")
+        ctime = int(os.path.getctime(self.path))
+        return datetime.datetime.fromtimestamp(ctime)
